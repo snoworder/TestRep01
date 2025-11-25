d2325ed960fe74860ba4af05f5f566afd71444f6
snoworder <64746544+snoworder@users.noreply.github.com>
2025-11-25 21:06:25 +0100

    any other ways?

--- temp.py
+++ temp.py
@@ -10,6 +10,7 @@ from xbbg import blp
 blp.bdp(tickers='NVDA US Equity', flds=['Security_Name', 'PX_LAST', 'GICS_Sector_Name'])
 blp.bdh(tickers='NVDA US Equity', flds=['PX_LAST'], start_date='2018-10-10', end_date='2018-10-20')
 
+# 3 other options?
 
 
 
