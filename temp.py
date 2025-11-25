307a5817a753c9f3df08d1cef6da81e66795e378
snoworder <snoworder@gmail.com>
2025-11-25 21:08:36 +0100

    3rd option??

--- temp.py
+++ temp.py
@@ -1,15 +1,16 @@
-#blpapi
+#1 blpapi
 pip install --index-url=https://bcms.bloomberg.com/pip/simple blpapi
 import blpapi
 
 
-#xbbg
+#2 xbbg
 pip install xbbg
 from xbbg import blp
 
 blp.bdp(tickers='NVDA US Equity', flds=['Security_Name', 'PX_LAST', 'GICS_Sector_Name'])
 blp.bdh(tickers='NVDA US Equity', flds=['PX_LAST'], start_date='2018-10-10', end_date='2018-10-20')
 
+# 3rd way?
 
 
 
